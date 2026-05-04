ae94cd5c104d9a1217a0c620f56c1fe3873a1256
Okalisteur <tsantarandrianarison@gmail.com>
2026-05-04 13:57:25 +0300

    Test pipeline

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Java Maven Project!");
+        // System.out.println("Hello from Java Maven Project!");
+        System.out.println("Test to see the pipeline");
     }
 }
